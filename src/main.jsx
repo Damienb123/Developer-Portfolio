40086d8787fbe7b943d6c0cec3fa95ef68dbbe61
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2025-08-11 21:02:03 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { ThemeProvider } from './common/ThemeContext.jsx';
-import { BrowserRouter } from 'react-router-dom'; 
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -14,3 +14,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </ThemeProvider>
   </React.StrictMode>
 );
+
