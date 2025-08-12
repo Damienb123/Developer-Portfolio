7f9453c1c4f799736e25ac353bd601273ea0ed3c
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2025-08-11 21:07:23 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,9 @@ import { ThemeProvider } from './common/ThemeContext.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
         <App />
-      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
+
 
