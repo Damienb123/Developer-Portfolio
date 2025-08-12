869d5091303fc31a19986f2d79eb72f15ce5d2f6
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2025-08-11 21:57:25 -0500

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/profile-img.png';
+import heroImg from '../../assets/image.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import githubLight from '../../assets/github-light.svg';
@@ -62,4 +62,5 @@ function Hero() {
 }
 
 export default Hero;
+
 
