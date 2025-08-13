f1310ce65b51a006aa7444aa24630b7ba59a8804
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2025-08-12 22:11:59 -0500

    Update App.jsx
    Framer Motion graphics for page scrolling progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import './App.css';
+import { Analytics } from '@vercel/analytics/react';
+
 import Footer from './sections/Footer/Footer';
 import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
-
+import { motion, useScroll } from 'framer-motion';
 
 function App() {
+
+  
+
+
+  const { scrollYProgress } = useScroll();
   return (
-    <>
-      <Hero />
-      <Projects />
-      <Skills />
-      <Footer />
-    </>
+
+    <Routes>
+     
+      {/* Home page route */}
+      <Route
+        path="/"
+        element={
+          <>
+            <motion.div
+        style={{
+          scaleX: scrollYProgress,
+          transformOrigin: '0%', // grow from left to right
+          height: '4px',
+          background: 'var(--scroll-color)',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 9999,
+        }}
+      />
+           <Analytics />
+            <Hero />
+            <Projects />
+            <Skills />
+            <Footer />
+          </>
+        }
+      />
+    </Routes>
   );
 }
 
 export default App;
-
-
