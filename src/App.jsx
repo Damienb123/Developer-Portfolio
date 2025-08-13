bd9fb85072bb56c0380f56715c51051ea3e7ba91
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2025-08-12 22:41:59 -0500

    Update App.jsx
    Framer Motion progression bar integrated for page section viewing

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,24 @@ import Hero from './sections/Hero/Hero';
 import Projects from './sections/Projects/Projects';
 import Skills from './sections/Skills/Skills';
 import { Analytics } from '@vercel/analytics/react';
-
+import { motion, useScroll } from 'framer-motion';
 
 function App() {
   return (
     <>
+        <motion.div
+        style={{
+          scaleX: scrollYProgress,
+          transformOrigin: '0%', // grow from left to right
+          height: '4px',
+          background: 'var(--scroll-color)',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          right: 0,
+          zIndex: 9999,
+        }}
+      />
       <Analytics />
       <Hero />
       <Projects />
@@ -19,6 +32,7 @@ function App() {
 }
 
 export default App;
+
 
 
 
