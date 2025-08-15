c93b3164fbbfcc54fbdbb4bb03538dd0209b5764
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2025-08-15 12:55:03 -0500

    Update Hero.jsx
    comments added for readability

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,4 +1,8 @@
+// Style import for Hero
 import styles from './HeroStyles.module.css';
+// ------------------------------------------
+// import for all images used
+// ------------------------------------------
 import heroImg from '../../assets/image.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
@@ -6,29 +10,34 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/My Resume.pdf'; // Updated import to Resume.pdf
+import CV from '../../assets/My Resume.pdf'; // Continuously updated
+// Use theme import for light and dark mode
+// ------------------------------------------
 import { useTheme } from '../../common/ThemeContext';
-
+// function call for hero content
 function Hero() {
+  // constant variable for theme and toggleTheme
   const { theme, toggleTheme } = useTheme();
-
+  // constant variables for light and dark transitions
   const themeIcon = theme === 'light' ? sun : moon;
   const githubIcon = theme === 'light' ? githubLight : githubDark;
   const linkedinIcon = theme === 'light' ? linkedinLight : linkedinDark;
 
   return (
+    // sectioned content for hero 
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
-        <img
+        
+        <img // img for profile picture
           src={heroImg}
           className={styles.hero}
           alt="Profile picture of Damien Beltran"
         />
         <img
-          className={styles.colorMode}
+          className={styles.colorMode} // colorMode for light and dark transitioning
           src={themeIcon}
           alt="Color mode icon"
-          onClick={toggleTheme}
+          onClick={toggleTheme} // toggle from light to dark mode
         />
       </div>
       <div className={styles.info}>
@@ -47,7 +56,7 @@ function Hero() {
             <img src={githubIcon} alt="Github icon" />
           </a>
           <a href="https://www.linkedin.com/in/damien-beltran-01a080222/" target="_blank">
-            <img src={linkedinIcon} alt="Linkedin icon" />
+            <img src={linkedinIcon} alt="Linkedin icon" /> 
           </a>
         </span>
         <p className= {styles.description}>
@@ -62,5 +71,6 @@ function Hero() {
 }
 
 export default Hero;
+
 
 
