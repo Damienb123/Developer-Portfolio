d2fecfec9a423c34f06e6d2a160ebfc94d1801e8
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2025-08-15 12:48:01 -0500

    Update Footer.jsx

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,7 +1,9 @@
+// Style import for Footer
 import styles from './FooterStyles.module.css';
-
+// Function for footer content
 function Footer() {
   return (
+    // Section for footer content containerized
     <section id="footer" className={styles.container}>
       <p>
         &copy; 2025 Damien Beltran. <br />
@@ -12,3 +14,4 @@ function Footer() {
 }
 
 export default Footer;
+
