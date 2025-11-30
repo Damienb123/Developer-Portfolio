32bf6450b1ae424da727aec1ca187132c6c41da8
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2025-11-30 15:47:54 -0600

    Update Hero.jsx

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import githubLight from '../../assets/github-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/SoftwareDevResume.pdf';
+import CV from '../../assets/MyResume.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
@@ -62,6 +62,7 @@ function Hero() {
 }
 
 export default Hero;
+
 
 
 
